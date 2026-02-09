86d31359d9b3be1d595bb235b6345750e3b18c5d
Hayden Hays <hjh2112@hsutx.edu>
2026-02-09 17:05:14 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = 3004;
+
+// Serve static files in this folder (css, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the HTML at /
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Privacy policy server running on port ${PORT}`);
+});
