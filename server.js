19c0c3d7a168f797b73f4b1eac728fc34f65203c
Hayden Hays <hayshj@yourdomain.com>
2026-02-12 17:57:38 +0000

    app-ads.txt

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get("/app-ads.txt", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'app-ads.txt'));
+});
+
 app.listen(PORT, () => {
   console.log(`Privacy policy server running on port ${PORT}`);
 });
